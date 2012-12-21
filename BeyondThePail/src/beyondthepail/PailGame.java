dfbab74546c8daec78907ac54caa2c0fc70ec790
Tom Coxon <tom.c.coxon@googlemail.com>
2012-12-21 13:10:35 +0000

    Add the raindrops. They also speed up and become more frequent as you play.

--- BeyondThePail/src/beyondthepail/PailGame.java
+++ BeyondThePail/src/beyondthepail/PailGame.java
@@ -1,5 +1,7 @@
 package beyondthepail;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input.Keys;
@@ -10,12 +12,15 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class PailGame implements ApplicationListener {
     static final int SPRITE_WIDTH = 48, SPRITE_HEIGHT = 48,
-            BUCKET_SPEED = 800;
+            BUCKET_SPEED = 800, RAIN_SPEED = 200;
     
     TextureRegion[][] spritesheet;
     TextureRegion dropImage, bucketImage;
@@ -26,6 +31,10 @@ public class PailGame implements ApplicationListener {
     SpriteBatch batch;
     
     Rectangle bucket;
+    Array<Rectangle> raindrops;
+    long lastDropTime,
+         rainSpeed, // # pixels per second the rain moves at
+         rainPeriod; // # ms between each raindrop
 	
 	@Override
 	public void create() {
@@ -51,10 +60,27 @@ public class PailGame implements ApplicationListener {
 	    bucket.height = SPRITE_HEIGHT;
 	    bucket.x = camera.viewportWidth / 2 - bucket.width / 2;
 	    bucket.y = 20;
+	    
+	    raindrops = new Array<Rectangle>();
+	    spawnRaindrop();
+	    rainSpeed = 200;
+	    rainPeriod = 1000;
 	}
 
 	@Override
 	public void dispose() {
+	}
+	
+	protected void spawnRaindrop() {
+	    Rectangle raindrop = new Rectangle();
+	    raindrop.x = MathUtils.random(camera.viewportWidth - SPRITE_WIDTH);
+	    raindrop.y = camera.viewportHeight;
+	    raindrop.width = SPRITE_WIDTH;
+	    raindrop.height = SPRITE_HEIGHT;
+	    raindrops.add(raindrop);
+	    lastDropTime = TimeUtils.millis();
+	    ++rainSpeed;
+	    --rainPeriod;
 	}
 	
 	protected void handleInput() {
@@ -75,6 +101,22 @@ public class PailGame implements ApplicationListener {
 	        bucket.x = camera.viewportWidth - bucket.width;
 	    
 	}
+	
+	protected void updateRain() {
+	    if (TimeUtils.millis()-lastDropTime > rainPeriod) spawnRaindrop();
+	    
+	    Iterator<Rectangle> iter = raindrops.iterator();
+	    while (iter.hasNext()) {
+	        Rectangle raindrop = iter.next();
+	        raindrop.y -= rainSpeed * Gdx.graphics.getDeltaTime();
+	        if (raindrop.y + raindrop.height < 0) iter.remove();
+	        
+	        if (raindrop.overlaps(bucket)) {
+	            dropSound.play();
+	            iter.remove();
+	        }
+	    }
+	}
 
 	@Override
 	public void render() {
@@ -82,12 +124,16 @@ public class PailGame implements ApplicationListener {
 	    Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	    
 	    handleInput();
+	    updateRain();
 	    
 	    camera.update();
 	    
 	    batch.setProjectionMatrix(camera.combined);
 	    batch.begin();
 	    batch.draw(bucketImage, bucket.x, bucket.y);
+	    for (Rectangle drop: raindrops) {
+	        batch.draw(dropImage, drop.x, drop.y);
+	    }
 	    batch.end();
 	}
 
